920ac93fd47fe4669fe1fe61cd4f1f122c947dcb
Kareem Mohamed <kmohamed@loyalty.com>
2021-01-11 19:53:58 -0500

    Add sorting options/routes to backend

--- backend/controllers/player-controller.js
+++ backend/controllers/player-controller.js
@@ -7,26 +7,72 @@ getAllPlayers = async (req, res) => {
         } else if (!players.length) {
             return res
                 .status(404)
-                .json({ success: false, error: `Players not found` })
+                .json({ success: false, data: [], error: `Players not found` })
         }
         return res.status(200).json({ success: true, data: players })
     }).catch(err => console.log(err))
 }
 
-getPlayerByName = async (req, res) => {
-    await Player.find({ "Player": `/.*${req.params.name}.*/`}, (err, players) => {
+getPlayersByYards = async (req, res) => {
+    //Include id to stabilize the sort
+    await Player.find().sort({ "Yds": -1, "_id": 1 }).exec((err, players) => {
         if (err) {
             return res.status(500).json({ success: false, error: err })
         } else if (!players.length) {
             return res
                 .status(404)
-                .json({ success: false, error: 'Player not found' })
+                .json({ success: false, data: [], error: `Players not found`})
         }
         return res.status(200).json({ success: true, data: players })
     })
 }
 
+getPlayersByLongestRun = async (req, res) => {
+    //Include id to stabilize the sort
+    await Player.find().sort({ "Lng": -1, "_id": 1 }).exec((err, players) => {
+        if (err) {
+            return res.status(500).json({ success: false, error: err })
+        } else if (!players.length) {
+            return res
+                .status(404)
+                .json({ success: false, data: [], error: `Players not found`})
+        }
+        return res.status(200).json({ success: true, data: players })
+    })
+}
+
+getPlayersByTouchdowns = async (req, res) => {
+    //Include id to stabilize the sort
+    await Player.find().sort({ "TD": -1, "_id": 1 }).exec((err, players) => {
+        if (err) {
+            return res.status(500).json({ success: false, error: err })
+        } else if (!players.length) {
+            return res
+                .status(404)
+                .json({ success: false, data: [], error: `Players not found`})
+        }
+        return res.status(200).json({ success: true, data: players })
+    })
+}
+
+getPlayerByName = async (req, res) => {
+    let query = new RegExp((req.params.name).toLowerCase())
+    await Player.find({ "Player": { $regex: new RegExp(req.params.name, "i") }}, (err, players) => {
+        if (err) {
+            return res.status(500).json({ success: false, error: err })
+        } else if (!players.length) {
+            return res
+                .status(404)
+                .json({ success: false, data: [], error: `No player with the name "${req.params.name}" found` })
+        }
+        return res.status(200).json({ success: true, data: players })
+    }).catch(err => console.log(err))
+}
+
 module.exports = {
     getAllPlayers,
-    getPlayerByName
+    getPlayerByName,
+    getPlayersByYards,
+    getPlayersByLongestRun,
+    getPlayersByTouchdowns
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,9 @@ router.get('/', function(req, res) {
   res.send('Express Server running...');
 });
 router.get('/list-players', PlayerController.getAllPlayers);
-router.get('/player:name', PlayerController.getPlayerByName)
+router.get('/player/:name', PlayerController.getPlayerByName);
+router.get('/sort-players/yards', PlayerController.getPlayersByYards)
+router.get('/sort-players/longest-run', PlayerController.getPlayersByLongestRun)
+router.get('/sort-players/touchdowns', PlayerController.getPlayersByTouchdowns)
 
 module.exports = router;
